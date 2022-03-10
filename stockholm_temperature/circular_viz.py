284ca8503d0277a7bfa2a8a995adf185ca3db059
Baptiste Engel <baptisteengel@outlook.com>
2022-03-10 22:24:43 +0100

    Circular temperature graph through years

--- stockholm_temperature/circular_viz.py
+++ stockholm_temperature/circular_viz.py
@@ -4,6 +4,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib.dates import MonthLocator, DateFormatter
 import numpy as np
+import imageio
+
 
 import utils
 
@@ -16,33 +18,58 @@ all_time_min_temp = all_temp_df.min(axis=None).min()
 # r = np.random.rand(365,1) * max_temp - min_temp 
 theta = np.linspace(0, 2*np.pi, 365)
 
+images = []
 sns.set_style("white")
+with plt.ion():
+    fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+    start_year = 2010
+    end_year = 2021
+    for year in range(start_year, end_year+1):
 
+        ax.set_rlim(bottom=-50, top=30)
+        ax.set_rticks([-50, 0, 30])
 
-plt.ion()
-fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+        ax.set_xticks(np.pi/180. * np.linspace(0,  360, 12, endpoint=False))
 
-for i in range(10):
-    plt.clf()
+        ax.set_xticklabels(['janv', 'fev', 'mars', 'avril', 'mai', 'juin', 'juil.', 'août', 'sept.', 'oct.', 'nov.', 'déc.'])
 
-    ax.set_rlim(bottom=-50, top=30)
-    ax.set_rticks([-50, 0, 30])
+        ax.set_thetalim(0, 2*np.pi)
 
-    max_temps = all_temp_df["max_temp"]
-    min_temps = all_temp_df["min_temp"]
+        ax.grid(True)
+        ax.set_title(f"{year}", va='bottom', y=0.455, x=1.2)
 
-    ax.plot(theta, max_temps, label='Maximal temperature', color= sns.color_palette("Reds")[-2], alpha=0.7, linewidth=1)
-    ax.plot(theta, min_temps, label='Minimal temperature', color= sns.color_palette("Blues")[-2], alpha=0.7, linewidth=1)
+        fig.suptitle(f'Temperature evolution in Stockholm through years from {start_year} to {end_year}.')
 
-    ax.set_xticks(np.pi/180. * np.linspace(0,  360, 12, endpoint=False))
+        max_temps = all_temp_df["max_temp"]
+        min_temps = all_temp_df["min_temp"]
 
-    ax.set_xticklabels(['janv', 'fev', 'mars', 'avril', 'mai', 'juin', 'juil.', 'août', 'sept.', 'oct.', 'nov.', 'déc.'])
+        year_temp = all_temp_df[str(year)]
 
-    ax.set_thetalim(0, 2*np.pi)
+        ax.plot(theta, max_temps, label='Max. temp', color= sns.color_palette("Reds")[-2], alpha=0.7, linewidth=1)
+        ax.plot(theta, min_temps, label='Min. temp', color= sns.color_palette("Blues")[-2], alpha=0.7, linewidth=1)
+        ax.legend( loc="lower left", bbox_to_anchor=(0.9,0.9))
+        
 
-    ax.grid(True)
-    ax.set_title("Year:{}", va='bottom')
+        plt.xticks(alpha=0.5)
+        plt.yticks(alpha=0.5)
 
-    plt.draw()
-    plt.pause(0.1)
-    print("Pause")
+        for previous_year in range(year-5, year):
+            try:
+                previous_year_temp = all_temp_df[str(previous_year)]
+                ax.plot(theta, previous_year_temp, color= sns.color_palette("Greys")[-2], alpha=0.7, linewidth=0.2)
+            except:
+                pass
+        
+        
+        for day in range(0,365, 29):
+            ax.plot(theta[day:day+39], year_temp[day:day+39], color= sns.color_palette("Greens")[-2], alpha=0.7, linewidth=1)
+            fig.canvas.draw()
+            image = np.frombuffer(fig.canvas.tostring_rgb(), dtype='uint8')
+            image  = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+
+            images.append(image)
+            plt.pause(0.001)
+       
+        plt.cla()
+
+imageio.mimsave('./result/temp_evolution.gif', images, fps=15)
